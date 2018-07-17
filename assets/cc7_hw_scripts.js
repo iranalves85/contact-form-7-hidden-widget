4903f05cd75d2820bb828c8aca6cabd49d246766
iranalves85 <iranjosealves@gmail.com>
2018-07-17 01:59:00 +0000

    Added custom Javascript Event when submit form toadd content in widget

--- assets/cc7_hw_scripts.js
+++ assets/cc7_hw_scripts.js
@@ -15,8 +15,6 @@ if ($docStatus != 'uninitialized' || $docStatus != 'loading') {
 
         document.addEventListener(currentValue.event, function(event) {
 
-            console.log(currentValue);
-
             //Stop function if the ID not exist
             if (event.detail.contactFormId != currentValue.id ||
                 currentValue.id == undefined) {
@@ -24,18 +22,28 @@ if ($docStatus != 'uninitialized' || $docStatus != 'loading') {
                 return;
             }
 
-            //
+            //Stop function if widget_id is not defined
             if (currentValue.widget_id == '' ||
                 currentValue.widget_id == undefined) {
                 console.error('CC7_HW: The atribute "id" for widget is not defined! Verify if Wordpress save widget configuration sucessufully.');
                 return;
             }
 
+            //Create a custom event to future interation
+            var customEvent = new Event('cc7_hw_add_content_event');
+
             //Get certain widget element
             $widgetElement = document.querySelector('#' + currentValue.widget_id + '.widget.cc7_hw_widget');
 
-            //Add content for widget
-            $widgetElement.innerHTML = currentValue.content;
+            //Listen event and implement function to add content
+            $widgetElement.addEventListener('cc7_hw_add_content_event', function(e) {
+                console.log(e);
+                //Add content for certain widget
+                e.target.innerHTML = currentValue.content;
+            }, false);
+
+            //Fire event registered
+            $widgetElement.dispatchEvent(customEvent);
 
         }, false);
 
